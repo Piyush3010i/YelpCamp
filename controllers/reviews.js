7fcfe540abf4dafb6bb9cf9c51e0a01e2c6d4b39
Piyush Bharadwaj <Piyushbharadwaj.3010@gmail.com>
2026-06-08 21:27:51 +0530

    star functionality for reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,6 +4,12 @@ const Review = require("../models/review");
 // controller for posting reviews
 module.exports.postReview = async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    
+    // conditions for 0 rating
+    if (Number(req.body.review.rating) === 0) {
+      req.flash("error", "Please select a rating of at least 1 star!");
+      return res.redirect(`/campgrounds/${campground._id}`);
+    }
     const review = new Review(req.body.review);
     review.author = req.user._id;
     campground.reviews.push(review);
